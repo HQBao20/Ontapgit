6ce5ba58843a16f57d5e592275610b2b7be97442
hoang quoc bao <hoangquocbao20920@gmail.com>
2022-12-27 10:57:11 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(void)
 {
+    uint8_t u_a = 0;
+    uint8_t u_b = 0;
+
+    prinf("Nhap a: ");
+    scanf("%d", &u_a);
+    prinf("Nhap b: ");
+    scanf("%d", &u_b);
+
     return 0;
 }
